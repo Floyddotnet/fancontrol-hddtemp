8fbbeeddcd78e0a48223d5f4129925f785d84caf
root <root@nas2>
2017-08-29 22:55:16 +0200

    add CPU-temp sensor evaluation for better air flow in chassis

--- fancontrol-hddtemp.py
+++ fancontrol-hddtemp.py
@@ -16,14 +16,18 @@ def fn_exit():
 
 signal.signal(signal.SIGINT, signal_handler)
 
-INTERVALL = 1 #seconds
+INTERVALL = 10 #seconds
 DISKS = "ls /dev/sd[a-z]"
 
 PIDFILE="/var/run/fancontrol-hddtemp.pid"
 LOCATION = "/var/lib/fancontrol-hddtemp"
-FILE_MAX = LOCATION + "/fancontrol-hddtemp-maximum"
-FILE_AVG = LOCATION + "/fancontrol-hddtemp-average"
-FILE_DAT = LOCATION + "/fancontrol-hddtemp-lastupdate"
+FILE_HDD_MAX = LOCATION + "/fancontrol-hddtemp-hdd-maximum"
+FILE_HDD_AVG = LOCATION + "/fancontrol-hddtemp-hdd-average"
+FILE_LASTUPDATE = LOCATION + "/fancontrol-hddtemp-lastupdate"
+FILE_SENSOR = LOCATION + "/fancontrol-hddtemp-temp-sensor"
+
+SENSOR_CPU_TEMP = "/sys/devices/platform/coretemp.0/hwmon/hwmon1/temp1_input"
+SENSOR_CPU_MIN_TEMP = 45 #ignore CPU-sensor if lower than this value
 
 if not os.path.exists(LOCATION):
     os.makedirs(LOCATION)
@@ -35,10 +39,10 @@ if os.path.exists(PIDFILE):
 with open(PIDFILE, "w") as f: f.write(str(os.getpid()))
     
 while True:
-    MAX_TEMP = 0
-    SUM_TEMP = 0
-    AVG_TEMP = 0
-    COUNT_TEMP = 0
+    MAX_HDD_TEMP = 0
+    SUM_HDD_TEMP = 0
+    AVG_HDD_TEMP = 0
+    COUNT_HDD_TEMP = 0
         
     lsdisks = subprocess.getoutput(DISKS).splitlines()
     for disk in lsdisks:
@@ -48,16 +52,29 @@ while True:
         
         temp = int(tempOut)
         
-        MAX_TEMP = max(MAX_TEMP, int(temp))
-        SUM_TEMP += temp
-        COUNT_TEMP += 1
+        MAX_HDD_TEMP = max(MAX_HDD_TEMP, int(temp))
+        SUM_HDD_TEMP += temp
+        COUNT_HDD_TEMP += 1
     
-    AVG_TEMP = int(SUM_TEMP / COUNT_TEMP)
+    AVG_HDD_TEMP = int(SUM_HDD_TEMP / COUNT_HDD_TEMP)
     
-    with open(FILE_MAX, "w") as f: f.write(str(MAX_TEMP*1000))
-    with open(FILE_AVG, "w") as f: f.write(str(AVG_TEMP*1000))
-    with open(FILE_DAT, "w") as f: f.write(str(datetime.datetime.now().isoformat()))
+    with open(FILE_HDD_MAX, "w") as f: f.write(str(MAX_HDD_TEMP*1000))
+    with open(FILE_HDD_AVG, "w") as f: f.write(str(AVG_HDD_TEMP*1000))
+    with open(FILE_LASTUPDATE, "w") as f: f.write(str(datetime.datetime.now().isoformat()))
     
+    #GET CPU-TEMP
+    #CPU-Temp is used for better air-flow in chassis if hdd's are idle but cpu under heavy load
+    cpuTemp = 0
+    with open(SENSOR_CPU_TEMP, "r") as f: cpuTemp = int(int(f.read())/1000)
+
+    SENSOR_TEMP = MAX_HDD_TEMP
+
+    if SENSOR_CPU_MIN_TEMP <= cpuTemp:
+        SENSOR_TEMP = max(MAX_HDD_TEMP, cpuTemp)
+
+    with open(FILE_SENSOR, "w") as f: f.write(str(SENSOR_TEMP*1000))
+
+
     time.sleep( INTERVALL )
     
 fn_exit()    
