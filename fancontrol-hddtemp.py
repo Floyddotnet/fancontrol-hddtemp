ec75d19c54bd9b742c13a43c3ff6f3c38701f642
Peter Becker <floyd.net@gmail.com>
2017-08-30 21:43:31 +0200

    complete rewrite to control fan-pwm self

--- fancontrol-hddtemp.py
+++ fancontrol-hddtemp.py
@@ -5,18 +5,9 @@ import os
 import time
 import signal
 import datetime
+import atexit
 
-def signal_handler(signal, frame):  fn_exit()
-
-def fn_exit():
-    os.remove(PIDFILE)
-    
-    print("\nprogram exiting gracefully")
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
-
-INTERVALL = 10 #seconds
+INTERVALL = 1 #seconds
 DISKS = "ls /dev/sd[a-z]"
 
 PIDFILE="/var/run/fancontrol-hddtemp.pid"
@@ -29,16 +20,50 @@ FILE_SENSOR = LOCATION + "/fancontrol-hddtemp-temp-sensor"
 SENSOR_CPU_TEMP = "/sys/devices/platform/coretemp.0/hwmon/hwmon1/temp1_input"
 SENSOR_CPU_MIN_TEMP = 45 #ignore CPU-sensor if lower than this value
 
-if not os.path.exists(LOCATION):
-    os.makedirs(LOCATION)
+FAN = "/sys/devices/platform/it87.2608/hwmon/hwmon2/pwm2"
+FAN_MIN_PWM=100
+FAN_MAX_PWM=255
+FAN_MIN_TEMP=30
+FAN_MAX_TEMP=40
 
-if os.path.exists(PIDFILE):
-    print("File " + PIDFILE + " exists, is fancontrol-hddtemp already running?")
-    sys.exit(0)
+_FAN_PWM_PER_C = (FAN_MAX_PWM-FAN_MIN_PWM) / (FAN_MAX_TEMP-FAN_MIN_TEMP)
+print ("Config FAN_PWM_PER_C: " + str(_FAN_PWM_PER_C))
+
+#utils
+def fn_write_file(file,content):
+    with open(file, "w") as f: 
+        f.write(str(content))
+
+def fn_write_pidfile():
+    if not os.path.exists(LOCATION):
+        os.makedirs(LOCATION)
     
-with open(PIDFILE, "w") as f: f.write(str(os.getpid()))
+    if os.path.exists(PIDFILE):
+        print("File " + PIDFILE + " exists, is fancontrol-hddtemp already running?")
+        sys.exit(0)
+        
+    fn_write_file(PIDFILE, os.getpid())
+
+#singnal handler
+def signal_handler(signal, frame):  
+    fn_exit()
+
+@atexit.register
+def fn_exit():
+    set_fanpwmcontrol(False)
     
-while True:
+    if os.path.exists(PIDFILE):
+        os.remove(PIDFILE)
+            
+        print("\nprogram exiting gracefully")
+        sys.exit(0)
+
+signal.signal(signal.SIGABRT, signal_handler)
+signal.signal(signal.SIGINT,  signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+#impl
+def fn_temps():
     MAX_HDD_TEMP = 0
     SUM_HDD_TEMP = 0
     AVG_HDD_TEMP = 0
@@ -58,9 +83,9 @@ while True:
     
     AVG_HDD_TEMP = int(SUM_HDD_TEMP / COUNT_HDD_TEMP)
     
-    with open(FILE_HDD_MAX, "w") as f: f.write(str(MAX_HDD_TEMP*1000))
-    with open(FILE_HDD_AVG, "w") as f: f.write(str(AVG_HDD_TEMP*1000))
-    with open(FILE_LASTUPDATE, "w") as f: f.write(str(datetime.datetime.now().isoformat()))
+    fn_write_file(FILE_HDD_MAX, MAX_HDD_TEMP*1000)
+    fn_write_file(FILE_HDD_AVG, AVG_HDD_TEMP*1000)
+    fn_write_file(FILE_LASTUPDATE, datetime.datetime.now().isoformat())
     
     #GET CPU-TEMP
     #CPU-Temp is used for better air-flow in chassis if hdd's are idle but cpu under heavy load
@@ -72,10 +97,35 @@ while True:
     if SENSOR_CPU_MIN_TEMP <= cpuTemp:
         SENSOR_TEMP = max(MAX_HDD_TEMP, cpuTemp)
 
-    with open(FILE_SENSOR, "w") as f: f.write(str(SENSOR_TEMP*1000))
+    fn_write_file(FILE_SENSOR, SENSOR_TEMP*1000)
+        
+    return int(SENSOR_TEMP);
 
-
-    time.sleep( INTERVALL )
+def set_fanpwmcontrol(enable):
+    fn_write_file(FAN + "_enable", "1" if enable else "0")
+        
+def set_fanspeed(temp):
+    pwm = FAN_MIN_PWM
     
-fn_exit()    
+    if temp > FAN_MIN_TEMP:
+        pwm = min(255,min(FAN_MAX_PWM, FAN_MIN_PWM + ((temp-FAN_MIN_TEMP) * _FAN_PWM_PER_C) ))
+    
+    pwm = int(max(FAN_MIN_PWM, pwm))
+    
+    fn_write_file(FAN, pwm)
+    print (temp, str(pwm), _FAN_PWM_PER_C)
+
+
+try:
+    fn_write_pidfile()
+    
+    while True:
+        set_fanpwmcontrol(True)
+        
+        temp = fn_temps()
+        set_fanspeed(temp)
+    
+        time.sleep( INTERVALL )
+finally:
+    fn_exit()    
     
